1007398093fd2e1c88d09dda898c879e591b6e11
code-perceptkraft <code-perceptkraft>
2023-11-18 14:01:03 +0530

    React Router Link

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,18 @@
 import { Fragment } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
-        <div></div>
+        <Link className="logo-container" to="/">
+          <div>Logo</div>
+        </Link>
+        <div className="links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
     </Fragment>
