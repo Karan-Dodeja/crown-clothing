508d7ba14d1b9b52c4c5bd91a52b29b0c7265275
Karan Dodeja <kdodeja62@gmail.com>
2023-11-20 02:54:05 +0530

    Add Context and useContext hook for storing user data sign in

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useContext  } from "react";
+
+import { UserContext } from "../../context/user.context";
 
 import { createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword, signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
 
@@ -17,6 +19,8 @@ const SignInForm = () => {
 
     const { email, password } = formField;
 
+    const { setCurrentUser } = useContext(UserContext);
+
     const resetFormFields = () => {
         setFormField(defaultFormFields);
     }
@@ -30,8 +34,10 @@ const SignInForm = () => {
         event.preventDefault();
 
         try {
-            const response = await signInAuthUserWithEmailAndPassword(email, password);
-            console.log(response);
+            const {user} = await signInAuthUserWithEmailAndPassword(email, password);
+            
+            setCurrentUser(user);
+            
             resetFormFields();
         } catch (error) {
             switch(error.code) {
